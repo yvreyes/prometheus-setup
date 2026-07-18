511cef2b714726099012cb033ffde32e8005d410
Yuri Reyes <yurivreyes.dev@gmail.com>
2026-07-18 23:28:12 +0800

    initial Prometheus Hyprland configuration

---
+++ dotfiles/hypr/modules/monitors.lua
@@ -0,0 +1,12 @@
+------------------
+---- MONITORS ----
+------------------
+
+-- See https://wiki.hypr.land/Configuring/Basics/Monitors/
+hl.monitor({
+  output = "DP-2",
+  mode = "1920x1080@180",
+  position = "auto",
+  scale = 1,
+})
+
